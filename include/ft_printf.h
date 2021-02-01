0abe79879bc4e784d36443af45f2563c7913b443
Leonardo Rocca <lrocca@student.42roma.it>
2021-02-01 19:02:07 +0100

    Added first structure.

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 18:50:12 by lrocca            #+#    #+#             */
-/*   Updated: 2021/01/29 17:03:29 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/02/01 18:42:41 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,18 @@
 # include <stdarg.h>
 # include "../libft/libft.h"
 
-int	ft_printf(const char *f, ...);
+int				ft_printf(const char *f, ...);
+
+typedef	struct	s_opt
+{
+	int		printed;
+	int		width;
+	int		precision;
+	int		minus;
+	int		padding;
+	va_list	args;
+}				t_opt;
+
+t_opt			*g_var;
 
 #endif
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,13 +6,45 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 18:24:21 by lrocca            #+#    #+#             */
-/*   Updated: 2021/01/29 16:23:42 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/02/01 19:01:39 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../include/ft_printf.h"
 
-int	ft_printf(const char *f, ...)
+int	validate(char *f)
 {
-	return (0);
+	g_var->printed = 0;
+	g_var->width = 0;
+	g_var->precision = 0;
+	g_var->minus = 0;
+	g_var->padding = ' ';
+	(void)f;
+	return (1);
+}
+
+int		ft_printf(const char *f, ...)
+{
+	t_opt	var;
+
+	if (!f)
+		return (0);
+	g_var = &var;
+	va_start(g_var->args, f);
+	while (*f)
+	{
+		if (*f == '%' && *(f + 1))
+		{
+			if (validate((char *)(f + 1)) == -1)
+				return (-1);
+		}
+		else
+		{
+			ft_putchar_fd(*f, 1);
+			g_var->printed++;
+		}
+		f++;
+	}
+	va_end(g_var->args);
+	return (g_var->printed);
 }
