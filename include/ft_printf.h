6f5e250e09767a315c91b7f0b70fb1ab1d58a49b
Leonardo Rocca <lrocca@student.42roma.it>
2021-02-06 19:34:54 +0100

    Removed <stdio> inclusion

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,14 +6,12 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 18:50:12 by lrocca            #+#    #+#             */
-/*   Updated: 2021/02/06 19:30:05 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/02/06 19:34:20 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
-# include <stdio.h>
 
 # include <stdarg.h>
 # include "../libft/libft.h"
